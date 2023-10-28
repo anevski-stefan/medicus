97091c7f585f71324c71b53331ee332ef9e5174a
Stefan Anevski <anevskistefan84@gmail.com>
2023-10-28 12:41:04 +0200

    Add home page

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./components/Navbar"
+import "../../public/styles/home.css"
+
+export default function Home() {
+  return (
+    <div className="bg flex w-full h-screen flex justify-center">
+      <div className="content-wrapper w-9/12">
+        <div className="navigation-wrapper">
+          <Navbar/>
+          <header className='header text-2xl text-center text-white leading-relaxed'>
+            <h1>
+              Welcome to your personal healtcare assistant. 
+            </h1>
+            <p>You have to register/sign in for you to use the application.</p>
+          </header>
+        </div>
+      </div>
+    </div>
+  )
+}
